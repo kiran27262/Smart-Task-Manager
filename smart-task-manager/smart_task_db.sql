9ce20cb7acfe54162c0735d0daf1a01bb0ced9e3
Kiran Kumar <csekirankumar1027@gmail.com>
2025-12-11 16:08:24 +0530

    Initial commit - Spring Boot backend

---
+++ smart-task-manager/smart_task_db.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE smart_task_db;
+SHOW DATABASES;
+ALTER USER 'your_user'@'localhost' IDENTIFIED WITH mysql_native_password BY 'your_password';
+FLUSH PRIVILEGES;
---
+++ smart-task-manager/src/main/java/com/example/SmartTaskManager/SmartTaskManagerApplication.java
@@ -0,0 +1,13 @@
+package com.app.smart.task.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartTaskManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartTaskManagerApplication.class, args);
+	}
+
+}
